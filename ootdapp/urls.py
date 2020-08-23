aa0330b5a07f7124a2b5ab12074101f12f0c54f7
Jeryong Ryu <wima90@gmail.com>
2020-08-23 23:44:31 +0900

    Add page

--- ootdapp/urls.py
+++ ootdapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('photo/<int:id>/', views.photo, name="photo"),
+    path('detail/', views.detail, name="detail"),
+    path('detail/<int:id>/', views.detail, name="detail"),
+    path('detail/<int:id>/<int:like>', views.detail, name="detail"),
     path('list/', views.list, name="list"),
 ]
--- ootdapp/views.py
+++ ootdapp/views.py
@@ -4,15 +4,30 @@ from django.template import loader
 from ootdapp.models import Sido, City, Image
 from django.core.paginator import Paginator
 
-def photo(request, id) :
-    page = request.GET.get('Image.id',id)
-    imageList = Image.objects.all()
-    paginator = Paginator(imageList,1)
-    imageListpage = paginator.get_page(page)
-    imgDetail = Image.objects.get(id=id)
-    context = { "imageList": imageListpage, "imgDetail":imgDetail }
+def detail(request, id, like) :
+    try :
+        page = request.GET.get('Image.id',id)
+        imageList = Image.objects.all()
+        paginator = Paginator(imageList,1)
+        imageListpage = paginator.get_page(page)
+        imgDetail = Image.objects.get(id=id)
+        context = { "imageList": imageListpage, "imgDetail": imgDetail }
+        if like == 0:
+            imgLike = Image.objects.get(id=id)
+            imgLike.image_like += 1
+            imgLike.save()
+            context = { "imageList": imageListpage, "imgDetail": imgDetail, "imgLike.image_like": imgLike.image_like }
+        elif like == 1:
+            imgDislike = Image.image_dislike
+            imgDislike.image_dislike += 1
+            imgDislike.save()
+            context = { "imageList": imageListpage, "imgDetail": imgDetail, "imgDislike.image_dislike": imgDislike.image_dislike }
 
-    return render(request, "photo.html", context)
+
+    except Image.DoesNotExist:
+        context = { "msg" : "게시물이 존재하지 않습니다." }
+
+    return render(request, "detail.html", context)
 
 
 def list(request) :
